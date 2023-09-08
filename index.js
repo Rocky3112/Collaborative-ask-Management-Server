8e7cf4ecbc748720eb7707f5f470b611bda5275b
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-09-08 21:21:40 +0600

    updated

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
   
-    await client.connect();
+    // await client.connect();
 
   
     const userCollection = client.db('taskDB').collection("users");
