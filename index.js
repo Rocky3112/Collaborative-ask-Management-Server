ac905bd25c3c3c66b620501c96781cf6409c6256
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-09-08 14:54:50 +0600

    modified code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 //middleware
 app.use(cors())
 app.use(express.json())
-
+//uri
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xn4aldo.mongodb.net/?retryWrites=true&w=majority`;
